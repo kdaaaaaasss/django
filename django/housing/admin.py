8c30d42ed92d2993ed440d3666a570ea7ebfc8d6
Студент Академии <std@itstep.loc>
2026-07-21 21:00:40 +0300

    commit 4 model

--- django/housing/admin.py
+++ django/housing/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+
+from housing.models import Housing, Photo
+
+
+
+admin.site.register(Housing)
+admin.site.register(Photo)
--- django/housing/models.py
+++ django/housing/models.py
@@ -1,3 +1,39 @@
+from random import choice
+
 from django.db import models
+
+# on_delete=models.CASCADE
+
+
+class Housing(models.Model):
+    # owner_id = models.ForeignKey(Users)
+    title = models.CharField(max_length=64, unique=True)
+    description = models.TextField(blank=True)
+    short_description = models.CharField(max_length=128, blank=True)
+    address = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=11, decimal_places=2)
+    rooms = models.PositiveIntegerField(default=1)
+    rating = models.PositiveIntegerField
+
+    def __str__(self):
+        return f"{self.title} | {self.rooms}"
+
+class Photo(models.Model):
+    # image = models.ImageField()
+    housing_id = models.ForeignKey(Housing, on_delete=models.CASCADE) #
+
+class Booking(models.Model):
+    # user_id =
+    # housing_id =
+    pass
+
+class Specifications(models.Model):
+    # housing_id
+    # specification_id
+    pass
+
+class Specification(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
 
 # Create your models here.
